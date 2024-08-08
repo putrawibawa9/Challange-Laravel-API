e417afff4fa703f996650dd1425976dddcb1cd2d
I Gede Putra Wibawa <putrawibawa7@gmail.com>
2024-08-08 14:41:38 +0800

    selesai membuat model order dan orderItem beserta migrasinya

---
+++ app/Models/Order.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\OrderItem;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Order extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'user_id', 
+        'total_price'
+    ];
+
+public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function items()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+}
