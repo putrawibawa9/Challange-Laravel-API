35e3d2f9bac8ea662c755981233b5308e8483412
I Gede Putra Wibawa <putrawibawa7@gmail.com>
2024-08-14 11:11:25 +0800

    berhasil migrasi dan model cart

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -20,4 +20,9 @@ class Product extends Model
     {
         return $this->hasMany(OrderItem::class);
     }
+
+      public function cartItems()
+    {
+        return $this->hasMany(CartItem::class);
+    }
 }
